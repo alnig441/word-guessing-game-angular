4c37313fa7634e21ae7fcdbfb9b103bbf83e8bd7
alnig441 <alnig441@gmail.com>
2022-05-10 18:26:40 -0400

    import modules update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,6 +28,7 @@ import { SetFocusDirective } from './directives/set-focus.directive';
     SetFocusDirective
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
